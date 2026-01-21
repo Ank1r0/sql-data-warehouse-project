3d49d0705b342ebaa6552dc835ed11c78b39b0c2
Illia Volchetskyi <81150210+Ank1r0@users.noreply.github.com>
2026-01-21 14:20:24 +0100

    material status fix

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,5 +1,7 @@
 
--- GOLD LAYER CUSTOMER DIMENSION
+
+use DataWarehouse
+GO
 CREATE VIEW gold.dim_customers AS
 SELECT 
 	ROW_NUMBER() OVER (ORDER BY cst_id) as customer_key,
@@ -8,7 +10,7 @@ SELECT
 	ci.cst_firstname as first_name,
 	ci.cst_lastname as last_name,
 	cl.cntry as country,
-	ci.cst_marital_status as marital_status,
+	ci.cst_material_status as material_status,
 	CASE WHEN ci.cst_gndr != 'n/a' then  ci.cst_gndr -- CRM is Maa=ster for gender
 			 ELSE COALESCE(ca.gen, 'n/a')
 		END as gender,
@@ -20,11 +22,7 @@ LEFT JOIN SILVER.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN SILVER.erp_loc_a101 cl
 on ci.cst_key = cl.cid;
-
-
---WILL BE MODIFIED LATER
-
---GOLD LAYER PRODUCT DIMENSION
+GO
 CREATE VIEW gold.dim_product as
 select 
 	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt,pn.prd_key) as product_key, -- product key for each product
@@ -43,8 +41,7 @@ from silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL; -- Filter out all historical data
-
-
+GO
 create view gold.fact_sales as
 SELECT
 	sd.sls_ord_num as order_number,
@@ -61,7 +58,6 @@ LEFT JOIN gold.dim_product pr
 ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
 ON sd.sls_cust_id = cu.customer_id;
-
 
 
 
